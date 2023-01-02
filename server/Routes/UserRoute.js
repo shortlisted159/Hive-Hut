cd2df74cd6fcd0b2b236c7cbd073a4ba302e9db5
Sanjukta Singha <64829176+shortlisted159@users.noreply.github.com>
2023-01-02 21:59:11 +0530

    Update UserRoute.js

--- server/Routes/UserRoute.js
+++ server/Routes/UserRoute.js
@@ -1,14 +1,14 @@
 import express from "express";
 import { deleteUser, followUser, getUser, UnfollowUser, updateUser, getAllUsers } from "../Controllers/UserController.js";
-import authMiddleware from "../MiddleWare/authMiddleWare.js";
+// import authMiddleware from "../MiddleWare/authMiddleWare.js";
 
 const router = express.Router();
 
 router.get('/', getAllUsers)
 router.get('/:id', getUser)
-router.put('/:id', authMiddleware, updateUser)
-router.delete('/:id',authMiddleware, deleteUser)
-router.put('/:id/follow',authMiddleware, followUser)
-router.put('/:id/unfollow',authMiddleware, UnfollowUser)
+router.put('/:id', updateUser)
+router.delete('/:id', deleteUser)
+router.put('/:id/follow', followUser)
+router.put('/:id/unfollow', UnfollowUser)
 
 export default router;
