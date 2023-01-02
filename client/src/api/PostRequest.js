3058f4921ed3919d4ebd94a4c8eaa9c9ad719f40
Sanjukta Singha <64829176+shortlisted159@users.noreply.github.com>
2023-01-02 22:44:33 +0530

    Update PostRequest.js

--- client/src/api/PostRequest.js
+++ client/src/api/PostRequest.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API = axios.create({baseURL : "http://localhost:5000"})
+const API = axios.create({baseURL : "https://hive-hutbackend.onrender.com/"})
 
  export const getTimelinePosts = (id)=> API.get(`/posts/${id}/timeline`)
  export const likePost = (id, userId)=> API.put(`post/${id}/like`, {userId: userId})
