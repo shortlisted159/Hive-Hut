4ed7e168b7248e4513f15ff517e33dc43b7d27ed
Sanjukta Singha <64829176+shortlisted159@users.noreply.github.com>
2023-02-18 12:09:59 +0530

    Update Posts.jsx

--- client/src/components/Posts/Posts.jsx
+++ client/src/components/Posts/Posts.jsx
@@ -21,6 +21,7 @@ const Posts = () => {
   if (params.id) posts = posts.filter((post) => post.userId === params.id)
   return (
     <div className="Posts">
+    // When a user logs in, this is displayed when the page loads.
       {loading
         ? "Fetching posts...."
         : posts.map((post, id) => {
